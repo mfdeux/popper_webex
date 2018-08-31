28fea54117987358f2b2c83e8c6dfdf873cbf3d1
Marc Ford <mrfxyz567@gmail.com>
2018-08-31 11:21:48 -0400

    Remove extraneous comments

--- src/resources/js/options.js
+++ src/resources/js/options.js
@@ -1,12 +1,9 @@
-// Saves options to chrome.storage
 const saveOptions = () => {
   let domain = document.getElementById('domain').value
   let browserAction = document.getElementById('browser-action').value
   chrome.storage.sync.set({ domain: domain, browserAction: browserAction })
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
 const restoreOptions = () => {
   chrome.storage.sync.get(['domain', 'browserAction'], function (data) {
     document.getElementById('domain').value = data.domain;
