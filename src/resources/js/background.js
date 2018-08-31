5ec88f998859f6a926e9ca571dfa9c89d78bf7b3
Marc Ford <mrfxyz567@gmail.com>
2018-08-31 11:27:12 -0400

    Remove extraneous functions

--- src/resources/js/background.js
+++ src/resources/js/background.js
@@ -150,16 +150,6 @@ const extractSciHubPDFUrl = async (articleUrl) => {
   }
 }
 
-// function openOptions() {
-//   if (chrome.runtime.openOptionsPage) {
-//     // New way to open options pages, if supported (Chrome 42+).
-//     chrome.runtime.openOptionsPage();
-//   } else {
-//     // Reasonable fallback.
-//     window.open(chrome.runtime.getURL('options.html'));
-//   }
-// }
-
 /**
  *
  * @param {*} articleUrl
